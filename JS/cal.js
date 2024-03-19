7ea746690a712e80434b1d014f0552a9091db771
Prince Singh Yadav <89747571+psysam@users.noreply.github.com>
2024-03-19 23:36:15 +0545

    Add files via upload

---
+++ JS/cal.js
@@ -0,0 +1,24 @@
+let string = "";
+let button = document.querySelectorAll('.button');
+let arr = Array.from(button).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == 'AC') {
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == 'DE') {
+            string = string.substring(0, string.length - 1);
+            document.querySelector('input').value = string;
+        }
+        else {
+            console.log(e.target);
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+
+    });
+});
